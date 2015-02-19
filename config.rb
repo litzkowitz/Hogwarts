deabaf70079b6a0bf86cc201ba6b68a79eedd959
litzkowitz <lisa.itzkowitz@gmail.com>
2015-02-18 21:12:06 -0600

    @import error in css

--- config.rb
+++ config.rb
@@ -1,12 +1,9 @@
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+http_path="/"
+css_dir="css"
+sass_dir="scss"
+images_dir="images"
+javascripts_dir="js"
 
-# You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+output_style=:expanded
+line_comments=false
